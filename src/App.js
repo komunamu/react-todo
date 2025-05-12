67d0640bd28cfe296e251b6aa905b5e6c9eb1e43
komunamu <komunamu@yahoo.com>
2025-05-11 22:53:41 -0400

    ToDoList-add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (event) => {
+    // prevent default behavior of the form eg. reloading the page
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    
+    //same ss function(currentArray) => 
+    setTodos(currentArray => [todo, ...currentArray]);
+    setTodo("");
+    console.log(todos);
+  
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" 
+          onChange={onChange}
+          value={todo} 
+          placeholder='write your to do...'/>
+          <button >Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
